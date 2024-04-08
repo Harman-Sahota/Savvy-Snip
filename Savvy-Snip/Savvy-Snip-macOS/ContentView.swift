fd3456562adb1ce85abdc9d9b2cd4d1a13adb07a
Harman-Sahota <10harmansahota@gmail.com>
2024-04-08 11:54:15 -0400

    macos login ui complete

--- Savvy-Snip/Savvy-Snip-macOS/ContentView.swift
+++ Savvy-Snip/Savvy-Snip-macOS/ContentView.swift
@@ -1,24 +1,82 @@
-//
-//  ContentView.swift
-//  Savvy-Snip-macOS
-//
-//  Created by Harman Sahota on 2024-04-08.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color("LaunchColor"))
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("LaunchImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .padding()
+                
+                Spacer()
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    HStack {
+                        Image(systemName: "envelope")
+                            .foregroundColor(.gray)
+                        TextField("Email", text: $email)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(height: 50)
+                            .controlSize(.extraLarge)
+                    }
+                    .padding(.horizontal)
+                    
+                    HStack {
+                        Image(systemName: "lock")
+                            .foregroundColor(.gray)
+                        SecureField("Password", text: $password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(height: 50)
+                            .controlSize(.extraLarge)
+                    }
+                    .padding(.horizontal)
+                    
+                    Button(action: {
+                        // Action code
+                    }) {
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .frame(width: 200,height: 40)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .textCase(.uppercase)
+                    }
+                    .buttonStyle(.plain)
+                    .padding(.horizontal)
+                    
+                    NavigationLink(destination: Text("Forgot your password?")) {
+                        Text("Forgot your password?")
+                            .foregroundColor(.blue)
+                            .font(.subheadline)
+                    }
+                    .padding(.top, 10)
+                    
+                    NavigationLink(destination: Text("Create An Account")) {
+                        Text("Create An Account")
+                            .foregroundColor(.blue)
+                            .font(.subheadline)
+                    }
+                    
+                    Spacer()
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
