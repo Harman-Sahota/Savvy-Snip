c344dc0928c1762bfb485b0fcd39dc7ede30af18
Harman-Sahota <10harmansahota@gmail.com>
2024-04-07 21:43:19 -0400

    getting started

---
+++ Savvy-Snip/Savvy-Snip/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Savvy-Snip
+//
+//  Created by Harman Sahota on 2024-04-07.
+//
+
+import SwiftUI
+import Cocoa
+
+struct K {
+    
+    static let darkBackgroundColor = Color(red: 15/255, green: 17/255, blue: 24/255)
+    static let lightBackgroundColor = Color(red: 255/255, green: 255/255, blue: 255/255)
+    
+    static let lightTextColor = Color(red: 15/255, green: 17/255, blue: 24/255)
+    static let darkTextColor = Color(red: 255/255, green: 255/255, blue: 255/255)
+}
+
+
+       
--- Savvy-Snip/Savvy-Snip/ContentView.swift
+++ Savvy-Snip/Savvy-Snip/ContentView.swift
@@ -1,24 +1,49 @@
-//
-//  ContentView.swift
-//  Savvy-Snip
-//
-//  Created by Harman Sahota on 2024-04-07.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(rectangleColor)
+                .edgesIgnoringSafeArea(.all) // Fill the entire window
+            
+            VStack {
+                
+                HStack {
+                    Text("Your Categories")
+                        .font(.title) // Set as a heading
+                        .foregroundColor(textColor) // Set text color
+                        .padding(.top, 40)
+                    // Add top padding
+                    
+                    Button("Add A New Category") {
+                        //do something
+                    }
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding(.top, 40)
+                }
+                
+                Spacer() // Spacer to push views to the top
+            }
         }
-        .padding()
+    }
+    
+    // Variables from Constants
+    var textColor: Color {
+        return colorScheme == .dark ? K.darkTextColor : K.lightTextColor
+    }
+    
+    var rectangleColor: Color {
+        return colorScheme == .dark ? K.darkBackgroundColor : K.lightBackgroundColor
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
