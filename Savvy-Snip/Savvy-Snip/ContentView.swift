a2be330275663aa709361dfed855af6f0bfd95e0
Harman-Sahota <10harmansahota@gmail.com>
2024-04-07 20:01:40 -0400

    switched to swift ui

---
+++ Savvy-Snip/Savvy-Snip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Savvy-Snip
+//
+//  Created by Harman Sahota on 2024-04-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Savvy-Snip/Savvy-Snip/Savvy_SnipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Savvy_SnipApp.swift
+//  Savvy-Snip
+//
+//  Created by Harman Sahota on 2024-04-07.
+//
+
+import SwiftUI
+
+@main
+struct Savvy_SnipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
