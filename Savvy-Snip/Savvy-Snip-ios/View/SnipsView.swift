f5fb69fac46379a42d0fd5f05012b2f10e716e72
Harman-Sahota <10harmansahota@gmail.com>
2024-04-23 12:52:49 -0400

    added add new snip button to the snips view

--- Savvy-Snip/Savvy-Snip-ios/View/SnipsView.swift
+++ Savvy-Snip/Savvy-Snip-ios/View/SnipsView.swift
@@ -4,8 +4,38 @@ struct SnipsView: View {
     let categoryName: String
     
     var body: some View {
-        Text("Snips for \(categoryName)")
-            .navigationBarTitle(categoryName)
+        VStack {
+            HStack {
+                //MARK: - Add Category Button
+                Button(action: {
+                    //                    isShowingAddCategorySheet = true
+                }) {
+                    HStack {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                        
+                        Text("Add A New Snip")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+                    .padding(.vertical, 2)
+                }
+                
+            }
+            .padding(.horizontal)
+            .padding(.top, 10)
+            
+            Spacer()
+            
+        }
+        .navigationBarTitle(categoryName, displayMode: .large)
+        
     }
 }
 
